75de5693e0ae39bf6ac49c566082b50a83fa4995
IgorMalyugin1 <90175900+IgorMalyugin1@users.noreply.github.com>
2023-07-19 21:05:17 +0300

    add selenoid

--- src/test/java/come/BaseTest.java
+++ src/test/java/come/BaseTest.java
@@ -4,6 +4,9 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.HashMap;
 
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
@@ -16,8 +19,19 @@ public class BaseTest {
         step("открываем главную страницу", () -> {
             open("https://www.wildberries.ru/");
         });
+
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", new HashMap<String, Object>() {{
+            put("enableVCN", true);
+            put("enableVideo", true);
+        }});
+
+
+        Configuration.browserCapabilities = capabilities;
     }
 
     @BeforeEach
